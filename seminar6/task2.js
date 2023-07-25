e7d0ec3db1b39447c6ecf6dd4b3934d2bca4ef92
Лилия <l_loshakova123@mail.ru>
2023-07-25 18:14:18 +0300

    Sketch

--- seminar6/task2.js
+++ seminar6/task2.js
@@ -1,30 +1,14 @@
-<!DOCTYPE html>
-<html lang="ru">
+let useMony = Number(prompt('Введите сумму вашего зароботка до вычита НДФЛ(13%)'));
+const salary = (mony) => mony * 0.87;
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seminar6</title>
-</head>
+function typeCheck(mony) {
+    if (isNaN(mony)) {
+        alert('Значение задано не верно!');
+        console.log('Значение задано не верно!');
+    } else {
+        alert(`Ваш заработок за вычетом НДФЛ, составляет: ${salary(useMony)}`);
+        console.log(`Ваш заработок за вычетом НДФЛ, составляет: ${salary(useMony)}`);
+    }
+}
 
-<body>
-
-    <script>
-        let useMony = Number(prompt('Введите сумму вашего зароботка до вычита НДФЛ(13%)'));
-        const salary = (mony) => mony * 0.87;
-
-        function typeCheck(mony) {
-            if (isNaN(mony)) {
-                alert('Значение задано не верно!');
-                console.log('Значение задано не верно!');
-            } else {
-                alert(`Ваш заработок за вычетом НДФЛ, составляет: ${salary(useMony)}`);
-                console.log(`Ваш заработок за вычетом НДФЛ, составляет: ${salary(useMony)}`);
-            }
-        }
-
-        typeCheck(useMony);
-    </script>
-</body>
-
-</html>
+typeCheck(useMony);
