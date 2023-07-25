479f8199b7048ebf18626c80b22effec20efe56f
Лилия <l_loshakova123@mail.ru>
2023-07-25 19:04:07 +0300

    Sketch

---
+++ seminar6/task4.js
@@ -0,0 +1,13 @@
+const buttonTask4Sum = () => {
+    let useNumA = Number(prompt('Введите первое число'));
+    let useNumB = Number(prompt('Введите второе число'));
+    const summa = (a, b) => a + b;
+    alert(`Сумма = ${summa(useNumA, useNumB)}`)
+    console.log(`Сумма = ${summa(useNumA, useNumB)}`);
+}
+
+
+// summa()
+// difference()
+// multiplication()
+// division()
