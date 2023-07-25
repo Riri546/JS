12ff73d850420bddf1b5801f2c0133e41db57c92
Лилия <l_loshakova123@mail.ru>
2023-07-25 18:13:18 +0300

    New file

---
+++ seminar6/task1.js
@@ -0,0 +1,4 @@
+alert('Есть числа 2 и 3. Возводим их в куб и складываем');
+const cubing = (useNum) => useNum ** 3;
+alert(`Итого, получаем: ${cubing(2) + cubing(3)}`);
+console.log(`Итого, получаем: ${cubing(2) + cubing(3)}`);
