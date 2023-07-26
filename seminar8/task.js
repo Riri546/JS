44e1ac271b874f00e2188507e3d9354f41de2cee
Лилия <l_loshakova123@mail.ru>
2023-07-26 20:47:29 +0300

    Sketch

---
+++ seminar8/task.js
@@ -0,0 +1,11 @@
+// Задание 1
+const task1 = () => {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+    }
+}
+
+// Задание 2
+const task2 = () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7]
+}
