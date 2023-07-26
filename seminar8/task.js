86c5d9eaa740de2da1d2dc11fb58166b91e4bfc9
Лилия <l_loshakova123@mail.ru>
2023-07-26 21:33:07 +0300

    Sketch

--- seminar8/task.js
+++ seminar8/task.js
@@ -1,13 +1,13 @@
 // Задание 1
 const task1 = () => {
-    function isEven(a) {
+    function even(a) {
         return a % 2 === 0;
     }
 
     for (let i = 0; i < 11; i++) {
         if (i === 0) {
             console.log(`${i} - это ноль`);
-        } else if (isEven(i)) {
+        } else if (even(i)) {
             console.log(`${i} - четное число`);
         } else {
             console.log(`${i} - нечетное число`);
