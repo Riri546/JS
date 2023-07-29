b7b535163bab64e16e096c8495ec5abeca627952
Лилия <l_loshakova123@mail.ru>
2023-07-29 20:45:21 +0300

    Sketch

--- seminar10/task.js
+++ seminar10/task.js
@@ -1,6 +1,14 @@
 // Задание 1
 const task1 = () => {
-
+    const numbers = {
+        keyin1: 1,
+        keyin2: 2,
+        keyin3: 3,
+        keyin4: 4,
+        keyin5: 5,
+        keyin6: 6,
+        keyin7: 7,
+    }
 }
 
 // Задание 2
