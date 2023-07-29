a6825e2f69623202da0914d8f0a772a02c3ddeec
Лилия <l_loshakova123@mail.ru>
2023-07-29 21:13:00 +0300

    Sketch

--- seminar10/task.js
+++ seminar10/task.js
@@ -10,8 +10,8 @@ const task1 = () => {
         keyin7: 7,
     }
 
+    console.log('Все значения больше или равные 3: ');
     for (const key in numbers) {
-
         if (numbers[key] >= 3) {
             console.log(numbers[key]);
         }
@@ -20,7 +20,30 @@ const task1 = () => {
 
 // Задание 2
 const task2 = () => {
-
+    const post = {
+        author: "John", // вывести этот текст
+        postId: 23,
+        comments: [
+            {
+                userId: 10,
+                userName: "Alex",
+                text: "lorem ipsum",
+                rating: {
+                    likes: 10,
+                    dislikes: 2, // вывести это число
+                },
+            },
+            {
+                userId: 5, // вывести это число
+                userName: "Jane",
+                text: "lorem ipsum 2", // вывести этот текст
+                rating: {
+                    likes: 3,
+                    dislikes: 1,
+                },
+            },
+        ],
+    };
 }
 
 // Задание 3
