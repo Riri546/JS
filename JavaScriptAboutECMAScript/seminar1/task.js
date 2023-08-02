285cfa81c4e99b56e29ed79c1fcfbc54863c6c08
Лилия <l_loshakova123@mail.ru>
2023-08-02 22:39:02 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar1/task.js
+++ JavaScriptAboutECMAScript/seminar1/task.js
@@ -5,6 +5,35 @@ const task1 = () => {
 }
 // Задание 2
 const task2 = () => {
+    const createCounter = () => {
+        let counter = 0;
+        const result = {
+            increment: function () {
+                return ++counter;
+            },
+            decrement: function () {
+                return --counter;
+            }
+        }
+
+        result.run = function () {
+            return ++counter;
+        };
+
+        const decrement = () => {
+            return --counter;
+        };
+
+
+
+        return increment(), decrement();
+    }
+
+    // Создаем счетчик.
+    const counter1 = createCounter();
+    console.log(counter1()); // 1
+    console.log(counter1());// 2
+    console.log(counter1());
 
 }
 
