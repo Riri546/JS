3683285e1d6608db31bf4dc1da092f6d6423f551
Лилия <l_loshakova123@mail.ru>
2023-08-02 14:43:51 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar1/task.js
+++ JavaScriptAboutECMAScript/seminar1/task.js
@@ -1,14 +1,15 @@
 // Задание 1
 const task1 = () => {
-
+    const arr = [1, 5, 7, 9];
+    console.log(Math.min(...arr));
 }
 // Задание 2
 const task2 = () => {
-    
+
 }
 
 // Задание 3
 const task3 = () => {
-    
+
 }
 
