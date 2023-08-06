2b386075ecea9cf8c51eed3d59763736234c14ef
Лилия <l_loshakova123@mail.ru>
2023-08-07 00:52:12 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar2/task.js
+++ JavaScriptAboutECMAScript/seminar2/task.js
@@ -69,50 +69,37 @@ const task2 = () => {
 
 // Задание 3
 const task3 = () => {
-    /* Создайте класс BankAccount, который представляет банковский счет.
-У него должны быть свойства accountNumber (номер счета) и balance
-(баланс), а также методы deposit(amount) для пополнения счета и
-withdraw(amount) для снятия денег со счета. Класс должен иметь
-также статическое свойство bankName, которое содержит название
-банка.
-const account1 = new BankAccount("1234567890", 1000);
-account1.deposit(500); // Вывод: Deposited 500 into account
-1234567890. New balance: 1500
-account1.withdraw(200); // Вывод: Withdrawn 200 from account
-1234567890. New balance: 1300
-account1.withdraw(1500); // Вывод: Insufficient funds in account
-1234567890 */
-
-    class Bank {
-        constructor (){
-
-        }
-
-        constructor(accountNumber, balance) {
-            this.accountNumber = accountNumber;
-            this.balance = balance;
-        }
-
-        bankName = 'Мой банк';
-
-        deposit(amount) {
-            this.balance += amount;
-            console.log(`Deposited ${amount} into account ${this.accountNumber}. New balance: ${this.balance}`);
-        }
-
-        withdraw(amount) {
-            if (amount > this.balance) {
-                console.log(`Insufficient funds in account ${this.accountNumber}`);
-            } else {
-                this.balance -= amount;
-                console.log(`Withdrawn ${amount} from account ${this.accountNumber}. New balance: ${this.balance}`);
-            }
+    class Bank{
+        constructor(name) {
+            this.name = name;
         }
     }
 
-    const account1 = new BankAccount('1234567890', 1000);
-    account1.deposit(500);
-    account1.withdraw(200);
-    account1.withdraw(1500);
+    class Client {
+        constructor(nameClient, ageClient) {
+            this.nameClient = nameClient;
+            this.ageClient = ageClient;
+        }
+
+        // deposit(amount) {
+        //     this.balance += amount;
+        //     console.log(`Deposited ${amount} into account ${this.accountNumber}. New balance: ${this.balance}`);
+        // }
+
+        // withdraw(amount) {
+        //     if (amount > this.balance) {
+        //         console.log(`Insufficient funds in account ${this.accountNumber}`);
+        //     } else {
+        //         this.balance -= amount;
+        //         console.log(`Withdrawn ${amount} from account ${this.accountNumber}. New balance: ${this.balance}`);
+        //     }
+        // }
+    }
+
+    const bank = new Bank('Мой Банк');
+    const client1  = new Client('Иван', 25);
+    client1.deposit(500);
+    client1.withdraw(200);
+    client1.withdraw(1500);
 }
 
