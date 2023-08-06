5aecf0e2dcdc3d89c79ba8804ef9b916a5b85695
Лилия <l_loshakova123@mail.ru>
2023-08-06 23:33:46 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar2/task.js
+++ JavaScriptAboutECMAScript/seminar2/task.js
@@ -1,6 +1,25 @@
 // Задание 1
 const task1 = () => {
-    
+    // Класс книги 
+    class Book {
+        // Свойства класса
+        title = 'Mara and Morok';
+        author = 'Leah Arden';
+        publishingHouse = 'Eksmo';
+        series = 'Hunters of the Worlds';
+        yearPublication = 2022;
+        pages = 352;
+
+        // Конструктор класса 
+        constructor(){
+
+        }
+
+        // Методы класса
+        displayinof(){
+            console.log(this);
+        }
+    }
 }
 
 // Задание 2
