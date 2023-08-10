1297ca3acd4fff5780e8c466270dcce41460643f
Лилия <l_loshakova123@mail.ru>
2023-08-11 00:00:22 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar3/task.js
+++ JavaScriptAboutECMAScript/seminar3/task.js
@@ -11,18 +11,18 @@ const task1 = () => {
     }
 
     class Manager extends Employee {
-        constructor(name,department) {
+        constructor(name, department) {
             super();
             this.name = name;
             this.department = department;
-          }
-        
+        }
+
         displayInfo = () => {
             console.log(`Name: ${this.name}`);
             console.log(`Department: ${this.department}`);
         }
-    }    
-    
+    }
+
     const employee = new Employee('John Smith')
     employee.displayInfo();
 
@@ -32,7 +32,38 @@ const task1 = () => {
 
 // Задание 2
 const task2 = () => {
+    class Product {
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+    
+    class Order {
+        product = this.addProducts;
 
+        constructor(orderNumber) {
+            this.orderNumber = orderNumber;
+        }
+
+        addProducts = () => {
+
+        }
+
+        getTotalPrice = () => {
+
+        }
+    }
+
+
+
+
+    const order = new Order(12345);
+    const product1 = new Product('Phone', 500);
+    order.addProduct(product1);
+    const product2 = new Product('Headphones', 100);
+    order.addProduct(product2);
+    console.log(order.getTotalPrice());
 }
 
 // Задание 3
