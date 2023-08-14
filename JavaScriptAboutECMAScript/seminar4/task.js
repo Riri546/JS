e23914266daac84654d4f16bcdbbd4168ed0a7a6
Лилия <l_loshakova123@mail.ru>
2023-08-14 15:51:43 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar4/task.js
+++ JavaScriptAboutECMAScript/seminar4/task.js
@@ -1,9 +1,48 @@
 // Задание 1
 const task1 = () => {
+    const getAsyncAwaitData = async (api) => {
+        const res = await fetch(api);
+        const json = await res.json();
+        localStorage.setItem('users', JSON.stringify(json)); // Сохраняем
+        return json;
+    };
 
+    const api = 'https://jsonplaceholder.typicode.com/users';
+
+
+    try {
+        const newData = await getAsyncAwaitData(api);
+        console.log(newData);
+        const pictureBoxEl = document.querySelector('.cards-box');
+        newData.forEach((element) => {
+            const picture = `
+              <div class="card" id="${element.id}"><!-- Добавили ID -->
+                  <h3>Пользователь ${element.id}</h3>
+                  <p class="name">Имя: ${element.name}</p>
+                  <p class="username">Ник: ${element.username}</p>
+                  <p class="email">Email: ${element.email}</p>
+                  <button class="btn__del">Удалить пользователя</button>
+              </div>
+              `
+
+            pictureBoxEl.insertAdjacentHTML("beforeend", picture)
+        })
+        const deleteBtn = document.querySelectorAll('.btn__del');
+        deleteBtn.forEach((button) => {
+            button.addEventListener('click', () => {
+                const product = button.closest('.card');
+                product.remove();
+                // Отфильтровали массив
+                const filterData = newData.filter(item => item.id !== product.id);
+                localStorage.setItem('users', JSON.stringify(filterData)); // Сохранили
+            })
+        })
+    } catch (error) {
+        console.error('что-то пошло не так');
+    }
 }
 
 // Задание 2
 const task2 = () => {
-    
+
 }
