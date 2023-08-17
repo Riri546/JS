5d37eb072b2f0ad011947db7ab3350c263fdc9a5
Лилия <l_loshakova123@mail.ru>
2023-08-17 22:11:42 +0300

    Sketch

--- frameworkVue/semitar2/script.js
+++ frameworkVue/semitar2/script.js
@@ -1,15 +1,25 @@
-const task1 = () =>{
-    // Get all cards
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card => {
-        // For each card
-        card.querySelectorAll("div > button").forEach(btn => {
-            // For each button on the card sides
-            btn.addEventListener("click", ev => {
-                // Toggle the "is-flipped" class of the button parent's parent
-                ev.target.parentElement.parentElement.classList.toggle("is-flipped");
-            });
-        });
-    });
-}
-
+// const task1 = () =>{
+//     // Get all cards
+//     const cards = document.querySelectorAll(".card");
+//     cards.forEach(card => {
+//         // For each card
+//         card.querySelectorAll("div > button").forEach(btn => {
+//             // For each button on the card sides
+//             btn.addEventListener("click", ev => {
+//                 // Toggle the "is-flipped" class of the button parent's parent
+//                 ev.target.parentElement.parentElement.classList.toggle("is-flipped");
+//             });
+//         });
+//     });
+// }
+const app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Привет, Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
