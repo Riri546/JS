2cbcda1df5fa15fe7c00efe2e48e1ddf159195be
Лилия <l_loshakova123@mail.ru>
2023-08-17 22:22:42 +0300

    Sketch

--- frameworkVue/semitar2/script.js
+++ frameworkVue/semitar2/script.js
@@ -1,27 +1,31 @@
 const app5 = new Vue({
-  el: '#app-1',
-  data: {
-    message: 'Привет, Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+    el: '#app-1',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
-  }
 })
 
 var app4 = new Vue({
     el: '#app-2',
     data: {
-      todos: [
-        { text: 'Изучить JavaScript' },
-        { text: 'Изучить Vue' },
-        { text: 'Создать что-нибудь классное' }
-      ]
+        todos: [
+            { text: 'Изучить JavaScript' },
+            { text: 'Изучить Vue' },
+            { text: 'Создать что-нибудь классное' }
+        ]
     },
     methods: {
         adding: function () {
             app4.todos.push({ text: 'Добавлен новый элемент' })
+        },
+        removing: function remove(el) {
+            var element = el;
+            element.remove();
         }
-      }
-  })
+    }
+})
