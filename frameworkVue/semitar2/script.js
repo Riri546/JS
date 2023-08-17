cead54c62ee66ff3b64433565cb2431baa3cbf4d
Лилия <l_loshakova123@mail.ru>
2023-08-17 16:37:47 +0300

    Sketch

--- frameworkVue/semitar2/script.js
+++ frameworkVue/semitar2/script.js
@@ -1,14 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Привет, Vue!'
+        message: 'Привет, Vue!'
     }
-  })
+})
 
 const task1 = new Vue({
     el: '#task1',
     data: {
         text: 'Lorem ipsum dolor sit amet.',
         button: 'Flip over'
+    },
+    method: {
+        flipOver(text){
+            this.text = text;
+            text = text.split("").reverse().join("");
+        }
     }
+    // text = 'abcd';
+    // str = 
 })
