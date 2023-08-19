bd408d88e24470fb933942129510c66e8a8ca800
Лилия <l_loshakova123@mail.ru>
2023-08-20 00:43:54 +0300

    Sketch

--- frameworkVue/seminar4/script.js
+++ frameworkVue/seminar4/script.js
@@ -1,3 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        sortParam: '',
+        phones: [
+            { title: 'Galaxy S8', company: 'Samsung', price: 45000 },
+            { title: 'iPhone 7', company: 'Apple', price: 49000 },
+            { title: 'Nokia N8', company: 'HMD Global', price: 25000 },
+            { title: 'Galaxy Note 8', company: 'Samsung', price: 50000 },
+            { title: 'iPhone 8', company: 'Apple', price: 60000 }]
+    },
+    computed: {
+        sortedList() {
+            switch (this.sortParam) {
+                case 'title': return this.phones.sort(sortByTitle);
+                case 'company': return this.phones.sort(sortByCompany);
+                case 'price': return this.phones.sort(sortByPrice);
+                default: return this.phones;
+            }
+        }
+    }
+});
+
+
+// cards.sort((a, b) => a.details.price - b.details.price);
+var sortByTitle = function () { return (d1.title.toLowerCase() > d2.title.toLowerCase()) ? 1 : -1; };
+var sortByCompany = function (d1, d2) { return (d1.company.toLowerCase() > d2.company.toLowerCase()) ? 1 : -1; };
+var sortByPrice = function (a, b) { return (a.price > b.price) ? 1 : -1; };
+
+
+
 // const products = [
 //     {
 //         name: 'Куртка',
@@ -30,75 +61,75 @@
 
 // console.log(products.sort((a, b) => a.details.price - b.details.price));
 
-const app1 = new Vue({
-    el: '#app-1',
-    data: {
-        sortRules: [
-            { key: 'good_id:asc', title: 'По порядку' },
-            { key: 'rating:desc', title: 'По рейтингу' },
-            { key: 'price:asc', title: 'По цене, сначала дешевые' },
-            { key: 'price:desc', title: 'По цене, сначала дорогие' }
-        ],
-        products: [
-            {
-                name: 'Джинсы',
-                details: {
-                    price: 30,
-                    size: 'small'
-                },
-            },
-            {
-                name: 'Куртка',
-                details: {
-                    price: 50,
-                    size: 'large'
-                },
-            },
-            {
-                name: 'Футболка',
-                details: {
-                    price: 20,
-                    size: 'medium'
-                },
-            },
-        ]
-    },
-    methods: {
-        filteredProducts: function () {
-            // Фильтруем товары
-            var filtered = this.products
-                // По категории
-                .filter(product => {
-                    return this.selectCategory == 0 || product.category_id == this.selectCategory;
-                })
+// const app1 = new Vue({
+//     el: '#app-1',
+//     data: {
+//         sortRules: [
+//             { key: 'good_id:asc', title: 'По порядку' },
+//             { key: 'rating:desc', title: 'По рейтингу' },
+//             { key: 'price:asc', title: 'По цене, сначала дешевые' },
+//             { key: 'price:desc', title: 'По цене, сначала дорогие' }
+//         ],
+//         products: [
+//             {
+//                 name: 'Джинсы',
+//                 details: {
+//                     price: 30,
+//                     size: 'small'
+//                 },
+//             },
+//             {
+//                 name: 'Куртка',
+//                 details: {
+//                     price: 50,
+//                     size: 'large'
+//                 },
+//             },
+//             {
+//                 name: 'Футболка',
+//                 details: {
+//                     price: 20,
+//                     size: 'medium'
+//                 },
+//             },
+//         ]
+//     },
+//     methods: {
+//         filteredProducts: function () {
+//             // Фильтруем товары
+//             var filtered = this.products
+//                 // По категории
+//                 .filter(product => {
+//                     return this.selectCategory == 0 || product.category_id == this.selectCategory;
+//                 })
 
-                // По брендам
-                .filter(product => {
-                    return this.selectBrand == 0 || product.brand == this.selectBrand;
-                })
+//                 // По брендам
+//                 .filter(product => {
+//                     return this.selectBrand == 0 || product.brand == this.selectBrand;
+//                 })
 
-                // По ценам
-                .filter(product => {
-                    return Number(product.price) >= this.minPrice && Number(product.price) <= this.maxPrice;
-                })
+//                 // По ценам
+//                 .filter(product => {
+//                     return Number(product.price) >= this.minPrice && Number(product.price) <= this.maxPrice;
+//                 })
 
-                // По полю поиска
-                .filter(product => {
-                    return this.inputSearch == '' || product.good.toLowerCase().indexOf(this.inputSearch.toLowerCase()) !== -1;
-                });
+//                 // По полю поиска
+//                 .filter(product => {
+//                     return this.inputSearch == '' || product.good.toLowerCase().indexOf(this.inputSearch.toLowerCase()) !== -1;
+//                 });
 
-            // Сортируем
-            var sorted = _.sortBy(filtered, product => {
-                return Number(product[this.sortKey]);
-            });
+//             // Сортируем
+//             var sorted = _.sortBy(filtered, product => {
+//                 return Number(product[this.sortKey]);
+//             });
 
-            // При необходимости сортируем в обратном направлении
-            if (this.sortDir === 'desc') {
-                sorted = sorted.reverse();
-            }
+//             // При необходимости сортируем в обратном направлении
+//             if (this.sortDir === 'desc') {
+//                 sorted = sorted.reverse();
+//             }
 
-            return sorted;
-        }
+//             return sorted;
+//         }
 
 
         // sortSize: function () {
@@ -108,12 +139,12 @@ const app1 = new Vue({
         // sortPricesAscendingOrder: function(){
         //     products.sort((a, b) => a.details.price - b.details.price)
         // }
-    }
-})
+//     }
+// })
 
 
-const customSortOrder = {
-    small: 1,
-    medium: 2,
-    large: 3,
-};
+// const customSortOrder = {
+//     small: 1,
+//     medium: 2,
+//     large: 3,
+// };
