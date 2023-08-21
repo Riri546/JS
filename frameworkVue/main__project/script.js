81b6dd98d5961695a19a2b6dbc431ecd16ab80e0
Лилия <l_loshakova123@mail.ru>
2023-08-21 16:23:55 +0300

    Sketch

--- frameworkVue/main__project/script.js
+++ frameworkVue/main__project/script.js
@@ -1,47 +1,51 @@
 var app1 = new Vue({
     el: '#app-1',
     data: {
-        newsArticles: [
-            {
-                post1: [
-                    {
-                        topic: 'Kitchan Design',
-                        title: 'Let’s Get Solution For Building Construction Work',
-                        date: '26 December,2022',
-                    }
-                ],
-                post2: [
-                    {
-                        topic: 'Kitchan Design',
-                        title: 'Let’s Get Solution For Building Construction Work',
-                        date: '26 December,2022',
-                    }
-                ]
-            },
-        ]
+        newsArticles: {
+            topics: [
+                { idTopic1: 'Kitchan Design' },
+                { idTopic2: 'Living Design' },
+                { idTopic3: 'Interior Design' },
+            ]
+        }
+
+
+        // newsArticles: [
+
+        //     {
+        //         topic: {},
+        //         title: addTitle(),
+        //         date: addDate(),
+        //     },
+        // ],
+        // topics: [
+        //     {idTopic1: 'Kitchan Design'},
+        //     {idTopic2: 'Living Design'},
+        //     {idTopic3: 'Interior Design'},
+        // ],
+
+        // methods: {
+        //     addTopic: function () {
+        //         app1.newsArticles.topic.push({idTopic1: 'Kitchan Design'})
+        //     }
+        // },
     }
 })
 
-
+// post1: [
+    //     {
+    //         topic: 'Kitchan Design',
+    //         title: 'Let’s Get Solution For Building Construction Work',
+    //         date: '26 December,2022',
+    //     }
+    // ],
+    // post2: [
+    //     {
+    //         topic: 'Living Design',
+    //         title: 'Let’s Get Solution For Building Construction Work',
+    //         date: '26 December,2022',
+    //     }
+    // ]
 //     posts() {
     // return { topics: ['Kitchan Design'] }
 // }
-// , 'Living Design', 'Interior Design'
-// newsArticles: [
-        //     {
-        //         post1: [
-        //             {
-        //                 topic: 'Kitchan Design',
-        //                 title: 'Let’s Get Solution For Building Construction Work',
-        //                 date: '26 December,2022',
-        //             }
-        //         ],
-        //         post2: [
-        //             {
-        //                 topic: 'Kitchan Design',
-        //                 title: 'Let’s Get Solution For Building Construction Work',
-        //                 date: '26 December,2022',
-        //             }
-        //         ]
-        //     },
-        // ]
