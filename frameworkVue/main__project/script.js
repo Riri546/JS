02e7087c33c2f235cc081bcf156de5663e55ab59
Лилия <l_loshakova123@mail.ru>
2023-08-21 14:32:03 +0300

    Sketch

---
+++ frameworkVue/main__project/script.js
@@ -0,0 +1,18 @@
+var app1 = new Vue({
+    el: '#app-1',
+    data: {
+        topicTexts: [
+            { text: 'Kitchan Design' },
+            { text: 'Изучить Vue' },
+            { text: 'Создать что-нибудь классное' }
+        ]
+    },
+    // methods: {
+    //     adding: function () {
+    //         app2.todos.push({ text: 'Добавлен новый элемент' })
+    //     },
+    //     remove: function(index) {
+    //         this.todos.splice(index, 1);
+    //       }
+    // }
+})
