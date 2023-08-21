06ef77aa957a30f19f9c5d73fcbdb6a6f71fcc6a
Лилия <l_loshakova123@mail.ru>
2023-08-21 16:04:58 +0300

    Sketch

--- frameworkVue/main__project/script.js
+++ frameworkVue/main__project/script.js
@@ -1,13 +1,31 @@
 var app1 = new Vue({
-    // el: '#app-1',
-    // data: {
-        posts (){
-            return { topics: ['Kitchan Design']}
-        }
-        
-    },
-)
+    el: '#app-1',
+    data: {
+        newsArticles: [
+            {
+                post1: [
+                    {
+                        topic: 'Kitchan Design',
+                        title: 'Let’s Get Solution For Building Construction Work',
+                        date: '26 December,2022',
+                    }
+                ],
+                post2: [
+                    {
+                        topic: 'Kitchan Design',
+                        title: 'Let’s Get Solution For Building Construction Work',
+                        date: '26 December,2022',
+                    }
+                ]
+            },
+        ]
+    }
+})
 
+
+//     posts() {
+    // return { topics: ['Kitchan Design'] }
+// }
 // , 'Living Design', 'Interior Design'
 // newsArticles: [
         //     {
