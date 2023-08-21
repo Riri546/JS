f78ac549a311acee9f1f54c505c668f051aa6991
Лилия <l_loshakova123@mail.ru>
2023-08-21 15:26:50 +0300

    Sketch

--- frameworkVue/main__project/script.js
+++ frameworkVue/main__project/script.js
@@ -1,15 +1,34 @@
 var app1 = new Vue({
     el: '#app-1',
     data: {
-        // new.
-        // ]
+        newsArticles: [
+            {
+                post1: [
+                    {
+                        topic: 'Kitchan Design',
+                        title: 'Let’s Get Solution For Building Construction Work',
+                        date: '26 December,2022',
+                    }
+                ],
+                post2: [
+                    {
+                        topic: 'Kitchan Design',
+                        title: 'Let’s Get Solution For Building Construction Work',
+                        date: '26 December,2022',
+                    }
+                ]
+
+            },
+        ]
     },
     // methods: {
+    //     post1: function () {
+    //     }
     //     adding: function () {
     //         app2.todos.push({ text: 'Добавлен новый элемент' })
     //     },
     //     remove: function(index) {
     //         this.todos.splice(index, 1);
     //       }
-    // }
+// }
 })
