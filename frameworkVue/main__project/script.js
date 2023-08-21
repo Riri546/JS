c8e5eead4698316cca726b1cf05e4e02d0d43e60
Лилия <l_loshakova123@mail.ru>
2023-08-21 21:18:57 +0300

    Sketch

--- frameworkVue/main__project/script.js
+++ frameworkVue/main__project/script.js
@@ -1,20 +1,17 @@
-// Работает только с определенным id
 var app1 = new Vue({
     el: '#app-1',
-
     data: {
         linkHref: "blog.html",
         post1: [{
             topic: 'Kitchan Design',
             title: 'Let’s Get Solution For Building Construction Work',
             date: '26 December,2022',
-        }],
+        }]
     }
 });
 
 var app2 = new Vue({
     el: '#app-2',
-
     data: {
         linkHref: "blog.html",
         post2: [{
@@ -27,7 +24,6 @@ var app2 = new Vue({
 
 var app3 = new Vue({
     el: '#app-3',
-
     data: {
         linkHref: "blog.html",
         post3: [{
@@ -39,61 +35,25 @@ var app3 = new Vue({
 });
 
 
-
-// { id2: 'Living Design' },
-// { id3: 'Interior Design' },
-        // newsArticles: [
-
-        //     {
-        //         topic: {},
-        //         title: addTitle(),
-        //         date: addDate(),
-        //     },
-        // ],
-        // topics: [
-        //     {idTopic1: 'Kitchan Design'},
-        //     {idTopic2: 'Living Design'},
-        //     {idTopic3: 'Interior Design'},
-        // ],
-
-        // methods: {
-        //     addTopic: function () {
-        //         app1.newsArticles.topic.push({idTopic1: 'Kitchan Design'})
-        //     }
-        // },
-        // newsArticles: [
-
-        //     {
-        //         topic: {},
-        //         title: addTitle(),
-        //         date: addDate(),
-        //     },
-        // ],
-        // topics: [
-        //     {idTopic1: 'Kitchan Design'},
-        //     {idTopic2: 'Living Design'},
-        //     {idTopic3: 'Interior Design'},
-        // ],
-
-        // methods: {
-        //     addTopic: function () {
-        //         app1.newsArticles.topic.push({idTopic1: 'Kitchan Design'})
-        //     }
-        // },
-// post1: [
-    //     {
-    //         topic: 'Kitchan Design',
-    //         title: 'Let’s Get Solution For Building Construction Work',
-    //         date: '26 December,2022',
-    //     }
-    // ],
-    // post2: [
-    //     {
-    //         topic: 'Living Design',
-    //         title: 'Let’s Get Solution For Building Construction Work',
-    //         date: '26 December,2022',
-    //     }
-    // ]
-//     posts() {
-    // return { topics: ['Kitchan Design'] }
+// Хочу сделать так, что бы небыло зависимости от id
+// var postNews = {
+//     data() {
+//         return {
+//             post1: [{
+//                 topic: 'Kitchan Design',
+//                 title: 'Let’s Get Solution For Building Construction Work',
+//                 date: '26 December,2022',
+//             }],
+//             post2: [{
+//                 topic: 'Living Design',
+//                 title: 'Low Cost Latest Invented Interior Designing Ideas.',
+//                 date: '22 December,2022',
+//             }],
+//             post3: [{
+//                 topic: 'Interior Design',
+//                 title: 'Best For Any Office & Business Interior Solution',
+//                 date: '25 December,2022',
+//             }]
+//         }
+//     }
 // }
