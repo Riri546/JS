1117735b2c5285daba26170703a4ef6fed75e870
Лилия <l_loshakova123@mail.ru>
2023-08-21 22:49:54 +0300

    Sketch

--- frameworkVue/main__project/script.js
+++ frameworkVue/main__project/script.js
@@ -2,77 +2,19 @@ const app = {
     data() {
         return {
             articlesData: [
-                { id: 1, src: 'blog.html', title: 'Let’s Get Solution For Building Construction Work', date: '26 December,2022', topic: 'Kitchan Design' },
-                { id: 2, src: 'blog.html', title: 'Low Cost Latest Invented Interior Designing Ideas.', date: '22 December,2022', topic: 'Living Design' },
-                { id: 3, src: 'blog.html', title: 'Best For Any Office & Business Interior Solution', date: '25 December,2022', topic: 'Interior Design' },
-                { id: 4, src: 'blog.html', title: 'Let’s Get Solution For Building Construction Work', date: '26 December,2022', topic: 'Kitchan Design' },
-                { id: 5, src: 'blog.html', title: 'Low Cost Latest Invented Interior Designing Ideas.', date: '22 December,2022', topic: 'Living Design' },
-                { id: 6, src: 'blog.html', title: 'Best For Any Office & Business Interior Solution', date: '25 December,2022', topic: 'Interior Design' }
+                { id: 1, src: '../img/Image_blog1.svg', alt: 'interior', title: 'Let’s Get Solution For Building Construction Work', date: '26 December,2022', button: 'Kitchan Design' },
+                { id: 2, src: '../img/Image_blog2.svg', alt: 'interior', title: 'Low Cost Latest Invented Interior Designing Ideas.', date: '22 December,2022', button: 'Living Design' },
+                { id: 3, src: '../img/Image_blog3.svg', alt: 'interior', title: 'Best For Any Office & Business Interior Solution', date: '25 December,2022', button: 'Interior Design' },
+                { id: 4, src: '../img/Image_blog4.svg', alt: 'interior', title: 'Let’s Get Solution For Building Construction Work', date: '26 December,2022', button: 'Kitchan Design' },
+                { id: 5, src: '../img/Image_blog5.svg', alt: 'interior', title: 'Low Cost Latest Invented Interior Designing Ideas.', date: '22 December,2022', button: 'Living Design' },
+                { id: 6, src: '../img/Image_blog6.svg', alt: 'interior', title: 'Best For Any Office & Business Interior Solution', date: '25 December,2022', button: 'Interior Design' }
             ]
         };
-    }
+    },
+    methods: {
+
+    },
+
 };
-Vue.createApp(app).mount('#app-1');
 
-
-// var app1 = new Vue({
-//     el: '#app-1',
-//     data: {
-//         linkHref: "blog.html",
-//         post1: [{
-//             topic: 'Kitchan Design',
-//             title: 'Let’s Get Solution For Building Construction Work',
-//             date: '26 December,2022',
-//         }]
-//     }
-// });
-
-// var app2 = new Vue({
-//     el: '#app-2',
-//     data: {
-//         linkHref: "blog.html",
-//         post2: [{
-//             topic: 'Living Design',
-//             title: 'Low Cost Latest Invented Interior Designing Ideas.',
-//             date: '22 December,2022',
-//         }]
-//     }
-// });
-
-// var app3 = new Vue({
-//     el: '#app-3',
-//     data: {
-//         linkHref: "blog.html",
-//         post3: [{
-//             topic: 'Interior Design',
-//             title: 'Best For Any Office & Business Interior Solution',
-//             date: '25 December,2022',
-//         }]
-//     }
-// });
-
-
-
-// Хочу сделать так, что бы небыло зависимости от id
-
-// var postNews = {
-//     data() {
-//         return {
-//             post1: [{
-//                 topic: 'Kitchan Design',
-//                 title: 'Let’s Get Solution For Building Construction Work',
-//                 date: '26 December,2022',
-//             }],
-//             post2: [{
-//                 topic: 'Living Design',
-//                 title: 'Low Cost Latest Invented Interior Designing Ideas.',
-//                 date: '22 December,2022',
-//             }],
-//             post3: [{
-//                 topic: 'Interior Design',
-//                 title: 'Best For Any Office & Business Interior Solution',
-//                 date: '25 December,2022',
-//             }]
-//         }
-//     }
-// }
+Vue.createApp(app).mount('#cardbox-container');
