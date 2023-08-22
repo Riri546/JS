ab5dbddb597f7d4719eae8dbeea05241931a97b3
Лилия <l_loshakova123@mail.ru>
2023-08-22 19:26:39 +0300

    Sketch

---
+++ frameworkVue/main__project/script/blogDitails.js
@@ -0,0 +1,23 @@
+Vue.component('tags', {
+    props: {},
+
+    data() {
+        return {
+            // title: 'Tags',
+        }
+    },
+
+    template: `
+    <section class="tags">
+    <h2 class="tags__title">{{ title }}dffv</h2>
+</section>`,
+});
+
+{/* <div class="button">
+<button class="tags__button">Kitchen</button>
+<button class="tags__button">Bedroom</button>
+<button class="tags__button">Building</button>
+<button class="tags__button">Architecture</button>
+<button class="tags__button">Kitchen Planning</button>
+<button class="tags__button">Bedroom</button>
+</div> */}
