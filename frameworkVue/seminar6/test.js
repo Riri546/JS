6982df0cf549d43ad0bf477611dd8d753e3dda0c
Лилия <l_loshakova123@mail.ru>
2023-08-24 03:26:10 +0300

    Sketch

--- frameworkVue/seminar6/test.js
+++ frameworkVue/seminar6/test.js
@@ -1,11 +1,11 @@
-Vue.component('test', {
-    props: {},
+// Vue.component('test', {
+//     props: {},
 
-    data() {
-        return {
-            title: 'Hello world 1'
-        }
-    },
+//     data() {
+//         return {
+//             title: 'Hello world 1'
+//         }
+//     },
 
-    template: `<h2>{{ title }}</h2>`,
-});
+//     template: `<h2>{{ title }}</h2>`,
+// });
