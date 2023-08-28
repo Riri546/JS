66a5a48adde43b1c6b5c660d2a3ed34810081195
Лилия <l_loshakova123@mail.ru>
2023-08-28 19:13:47 +0300

    Sketch

--- frameworkVue/vue__main__project/src/App.vue
+++ frameworkVue/vue__main__project/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
