3cfa561af75f807f04d0c0bfa054b54e4368b122
Лилия <l_loshakova123@mail.ru>
2023-08-29 14:23:46 +0300

    Sketch

--- frameworkVue/vue__main__project/vue.config.js
+++ frameworkVue/vue__main__project/vue.config.js
@@ -2,3 +2,20 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  pages: {
+    'index': {
+      entry: './src/pages/Home/main.js',
+      template: 'public/index.html',
+      title: 'Home',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+    },
+    'blog': {
+      entry: './src/pages/About/main.js',
+      template: 'public/index.html',
+      title: 'Blog',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'blog' ]
+    }
+  }
+}
