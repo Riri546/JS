5ecbc679aa6e2611143a5097b812afb9faf31d2c
Лилия <l_loshakova123@mail.ru>
2023-08-29 14:11:03 +0300

    New file

---
+++ frameworkVue/vue__main__project/src/pages/blog/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HomePage/>
+    <BlogPage/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+import BlogPage from './components/BlogPage.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    BlogPage
+  }
+}
+</script>
+
+<style>
+/* #app {
+} */
+</style>
