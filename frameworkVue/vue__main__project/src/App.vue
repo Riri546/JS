be28eed48f28012159b56a41ee25b4c515de32e8
Лилия <l_loshakova123@mail.ru>
2023-08-30 00:17:54 +0300

    Sketch

--- frameworkVue/vue__main__project/src/App.vue
+++ frameworkVue/vue__main__project/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <HomePage/>
-    <BlogPage/>
+    <HomePage />
+    <BlogPage />
+    <BlogDitailsPage />
   </div>
 </template>
 
 <script>
 import HomePage from './components/HomePage.vue'
 import BlogPage from './components/BlogPage.vue'
+import BlogDitailsPage from './components/BlogDitailsPage.vue'
 
 
 export default {
   name: 'App',
   components: {
     HomePage,
-    BlogPage
+    BlogPage,
+    BlogDitailsPage
   }
 }
 </script>
