1b2a975cd67756a43d00c9580d70657c7fd9ee1b
Лилия <l_loshakova123@mail.ru>
2023-08-30 01:10:09 +0300

    Sketch

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="hello">
-    <img src="" alt="">
+    <h1>Hi</h1>
+    <img :src="'../../frameworkVue/main__project/img/' + src" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    
-  }
+  data() {
+    return {
+      src: 'Image_blog1.svg',
+    };
+  },
 }
 </script>
 
@@ -18,15 +21,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
-}
-</style>
+}</style>
