ee0455ce4d76df1b8536f06100e027e961f0df03
Лилия <l_loshakova123@mail.ru>
2023-08-29 14:26:06 +0300

    Sketch

--- frameworkVue/vue__main__project/vue.config.js
+++ frameworkVue/vue__main__project/vue.config.js
@@ -3,19 +3,19 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-module.exports = {
-  pages: {
-    'index': {
-      entry: './src/pages/Home/main.js',
-      template: 'public/index.html',
-      title: 'Home',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
-    },
-    'blog': {
-      entry: './src/pages/About/main.js',
-      template: 'public/index.html',
-      title: 'Blog',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'blog' ]
-    }
-  }
-}
+// module.exports = {
+//   pages: {
+//     'index': {
+//       entry: './src/pages/Home/main.js',
+//       template: 'public/index.html',
+//       title: 'Home',
+//       chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+//     },
+//     'blog': {
+//       entry: './src/pages/About/main.js',
+//       template: 'public/index.html',
+//       title: 'Blog',
+//       chunks: [ 'chunk-vendors', 'chunk-common', 'blog' ]
+//     }
+//   }
+// }
