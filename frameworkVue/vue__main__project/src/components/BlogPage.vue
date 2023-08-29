70f672b8014756d886bf19c3ce54ae2c7c2a3710
Лилия <l_loshakova123@mail.ru>
2023-08-29 14:05:01 +0300

    Sketch

--- frameworkVue/vue__main__project/src/components/BlogPage.vue
+++ frameworkVue/vue__main__project/src/components/BlogPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        
+        Hi
     </div>
 </template>
 
--- frameworkVue/vue__main__project/vue.config.js
+++ frameworkVue/vue__main__project/vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  pages: {
+    index: {
+    },
+    // когда использутся строковый формат точки входа, то
+    // шаблон будет определяться как `public/subpage.html`,
+    // а если таковой не будет найден, то `public/index.html`.
+    // Выходное имя файла будет определено как `subpage.html`.
+    blog: 'src/blog/main.js'
+  }
+}
