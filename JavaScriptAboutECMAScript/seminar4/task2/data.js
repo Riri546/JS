24eb6d93fa41771c5465409946e7704510aaf541
Лилия <l_loshakova123@mail.ru>
2023-09-02 18:05:13 +0300

    Sketch

--- JavaScriptAboutECMAScript/seminar4/task2/data.js
+++ JavaScriptAboutECMAScript/seminar4/task2/data.js
@@ -1,34 +1,34 @@
-// const content = document.querySelector('.content');
-// content.classList.add('center');
+const content = document.querySelector('.content');
+content.classList.add('center');
 
-// const contentItems = document.createElement('div');
-// contentItems.classList.add('content__items');
-// content.append(contentItems);
+const contentItems = document.createElement('div');
+contentItems.classList.add('content__items');
+content.append(contentItems);
 
-// let count = 0;
+let count = 0;
 
-// setTimeout(function getData() {
+setTimeout(function getData() {
 
-//     fetch('https://dog.ceo/api/breeds/image/random')
-//         .then(response => response.json())
-//         .then(dog => {
-//             const contentItem = document.createElement('div');
-//             contentItem.classList.add('item');
-//             contentItems.append(contentItem);
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(dog => {
+            const contentItem = document.createElement('div');
+            contentItem.classList.add('item');
+            contentItems.append(contentItem);
 
-//             const contentImage = document.createElement('img');
-//             contentImage.classList.add('item__image');
-//             contentImage.setAttribute('src', 'alt');
-//             contentImage.src = `${dog.message}`;
-//             contentImage.alt = 'item photo';
-//             contentItem.append(contentImage);
-//         })
-//         .catch((error) => console.log(error.message));
+            const contentImage = document.createElement('img');
+            contentImage.classList.add('item__image');
+            contentImage.setAttribute('src', 'alt');
+            contentImage.src = `${dog.message}`;
+            contentImage.alt = 'item photo';
+            contentItem.append(contentImage);
+        })
+        .catch((error) => console.log(error.message));
 
-//     count++;
+    count++;
 
-//     if (count < 10) {
-//         setTimeout(getData, 3000);
-//     }
-// }, 3000);
+    if (count < 10) {
+        setTimeout(getData, 3000);
+    }
+}, 3000);
 
