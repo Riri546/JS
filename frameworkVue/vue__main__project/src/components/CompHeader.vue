fe31e48425eea86b00f59458d6a1f0f38a1c2d98
Лилия <l_loshakova123@mail.ru>
2023-09-03 19:56:03 +0300

    Sketch

--- frameworkVue/vue__main__project/src/components/CompHeader.vue
+++ frameworkVue/vue__main__project/src/components/CompHeader.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="top">
-        <header class="head">
-            <nav class="head__navigation">
-                <!-- Логотип с сылкой на главную страницу -->
-                <div class="head__logo" v-for="head in head__logo" :key="head.id">
-                    <a :href="head.link"> <img src="../assets/img/logo.svg" :alt="head.alt"></a>
-                </div>
+    <header class="head">
+        <nav class="head__navigation">
+            <!-- Логотип с сылкой на главную страницу -->
+            <div class="head__logo" v-for="head in head__logo" :key="head.id">
+                <a :href="head.link"> <img src="../assets/img/logo.svg" :alt="head.alt"></a>
+            </div>
 
-                <!-- Иконки навигации -->
-                <div class="head__icon">
-                    <div v-for="head__icon in head__icons" :key="head__icon.id">
-                        <a class="head__icon_link" :href="head__icon.link">{{ head__icon.name }}</a>
-                    </div>
+            <!-- Иконки навигации -->
+            <div class="head__icon">
+                <div v-for="head__icon in head__icons" :key="head__icon.id">
+                    <a class="head__icon_link" :href="head__icon.link">{{ head__icon.name }}</a>
                 </div>
-            </nav>
-        </header>
-    </div>
+            </div>
+        </nav>
+    </header>
 </template>
   
 <script>
