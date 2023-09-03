f6b5e55424836fde091b0bda7c69e96acd8ed9e9
Лилия <l_loshakova123@mail.ru>
2023-09-03 21:05:12 +0300

    Sketch

---
+++ frameworkVue/seminar10/src/components/ProductDitails.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>
+            <img src="#" alt="photo">
+            <H1>Lorem ipsum dolor sit amet.</H1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa expedita dicta enim molestiae, inventore porro quae veniam architecto, repellendus quod dolores provident recusandae. Quam consectetur animi assumenda, blanditiis exercitationem illum!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JSProductDitails',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
