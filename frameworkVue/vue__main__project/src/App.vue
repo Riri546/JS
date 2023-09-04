cc4cc5076468c6d3b0cb28a05ac8802120922730
Лилия <l_loshakova123@mail.ru>
2023-09-04 23:29:44 +0300

    Sketch

--- frameworkVue/vue__main__project/src/App.vue
+++ frameworkVue/vue__main__project/src/App.vue
@@ -11,16 +11,18 @@
     <CompFooter />
     <CompHeader />
     <ProductDitailsPage />
+    <ProjectPage />
   </div>
 </template>
 
 <script>
 import CompHeader from './components/CompHeader.vue';
 import CompFooter from './components/CompFooter.vue';
-import HomePage from './components/HomePage.vue'
-import BlogPage from './components/BlogPage.vue'
-import BlogDitailsPage from './components/BlogDitailsPage.vue'
-import ProductDitailsPage from './components/ProductDitailsPage.vue'
+import HomePage from './components/HomePage.vue';
+import BlogPage from './components/BlogPage.vue';
+import BlogDitailsPage from './components/BlogDitailsPage.vue';
+import ProjectPage from './components/ProjectPage.vue';
+import ProductDitailsPage from './components/ProductDitailsPage.vue';
 
 export default {
   name: 'App',
@@ -30,6 +32,7 @@ export default {
     BlogPage,
     BlogDitailsPage,
     ProductDitailsPage,
+    ProjectPage,
     CompFooter
   }
 }
