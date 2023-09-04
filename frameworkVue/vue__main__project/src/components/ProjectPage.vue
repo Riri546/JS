b6af00cacfa5d12974ec6a7137b3fe092a310d22
Лилия <l_loshakova123@mail.ru>
2023-09-04 23:39:14 +0300

    Sketch

--- frameworkVue/vue__main__project/src/components/ProjectPage.vue
+++ frameworkVue/vue__main__project/src/components/ProjectPage.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        
+        <header class="subhead" v-for="subhead in subheader" :key="subhead.id">
+            <div class="subhead__img">
+                <img class="subhead__img_photo" src="../assets/img/blog/Image_laceholder.jpg" :alt="subhead.alt">
+                <span class="subhead__section">
+                    <h1 class="subhead__section_title">{{ subhead.title }}</h1>
+                    <div>
+                        <a class="subhead__link" :href="subhead.linl_htef_one">{{ subhead.link_name_one }}</a> / <a
+                            class="subhead__link" :href="subhead.linl_htef_two">{{ subhead.link_name_two }}</a>
+                    </div>
+                </span>
+            </div>
+        </header>
     </div>
 </template>
 
@@ -10,20 +21,20 @@ export default {
 
     data() {
         return {
-            
+            subheader: [
+                { src_img: '../assets/img/blog/Image_laceholder.jpg', alr: 'img blog ditails', title: 'Articles & News', linl_htef_one: 'index.html', link_name_one: 'Home', linl_htef_two: 'blogDitails.html', link_name_two: 'Blog' }],
+
         };
     },
 
     mounted() {
-        
+
     },
 
     methods: {
-        
+
     },
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
