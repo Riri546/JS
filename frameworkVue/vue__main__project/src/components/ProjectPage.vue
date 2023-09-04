636258a722c788864e865e8c712c5ba3086a2cbd
Лилия <l_loshakova123@mail.ru>
2023-09-04 23:50:49 +0300

    New file

--- frameworkVue/vue__main__project/src/components/ProjectPage.vue
+++ frameworkVue/vue__main__project/src/components/ProjectPage.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="subhead" v-for="subhead in subheader" :key="subhead.id">
             <div class="subhead__img">
-                <img class="subhead__img_photo" src="../assets/img/blog/Image_laceholder.jpg" :alt="subhead.alt">
+                <img class="subhead__img_photo" src="../assets/img/project/subhead.jpg" :alt="subhead.alt">
                 <span class="subhead__section">
                     <h1 class="subhead__section_title">{{ subhead.title }}</h1>
                     <div>
@@ -22,7 +22,7 @@ export default {
     data() {
         return {
             subheader: [
-                { src_img: '../assets/img/blog/Image_laceholder.jpg', alr: 'img blog ditails', title: 'Articles & News', linl_htef_one: 'index.html', link_name_one: 'Home', linl_htef_two: 'blogDitails.html', link_name_two: 'Blog' }],
+                { src_img: '../assets/img/blog/Image_laceholder.jpg', alr: 'img blog ditails', title: 'Articles & News', linl_htef_one: 'index.html', link_name_one: 'Home', linl_htef_two: 'blogDitails.html', link_name_two: 'Project' }],
 
         };
     },
