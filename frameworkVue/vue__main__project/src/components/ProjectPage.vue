f50248181c9670a5bbe1bdb0b0b526d95ef1236e
Лилия <l_loshakova123@mail.ru>
2023-09-07 17:17:45 +0300

    Sketch

--- frameworkVue/vue__main__project/src/components/ProjectPage.vue
+++ frameworkVue/vue__main__project/src/components/ProjectPage.vue
@@ -18,7 +18,23 @@
             <nav></nav>
             <nav></nav>
         </div>
-        
+        <section class="project">
+            <article class="project__catalog">
+                <img class="project__img" src="../assets/img/project/Photo (1).png" alt="photo">
+                <div class="project__about">
+                    <h1 class="project__title">Minimal Bedroom</h1>
+                    <a href="#">Decor</a>
+                    <span>/</span>
+                    <a href="#">Artchitecture</a>
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70"
+                            fill="none">
+                            <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+                            <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg></a>
+                </div>
+            </article>
+        </section>
     </div>
 </template>
 
