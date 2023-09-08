f2c219a5addda39a1ded80c182275771b09342e8
Лилия <l_loshakova123@mail.ru>
2023-09-08 17:01:23 +0300

    Sketch

---
+++ frameworkVue/vue__main__project/src/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="project__buttons_item">{{ text.name }}</div>
+    <!-- <div class="project__buttons_item">
+        <slot />
+    </div> -->
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.project__buttons_item {
+    display: flex;
+    padding: 26px 66px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    border-radius: 18px;
+    // background: #CDA274;
+    background: #FFF;
+
+    // color: #FFF;
+    color: #292F36;
+    text-align: center;
+    font-family: Jost;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 125%;
+    /* 22.5px */
+    letter-spacing: 0.36px;
+
+    &-active {
+        background: #CDA274;
+        color: #FFF;
+
+    }
+}
+</style>
