e1fda668da347b7485708f100691482987ed3368
Лилия <l_loshakova123@mail.ru>
2023-09-10 05:30:33 +0300

    Sketch

---
+++ frameworkVue/vue__main__project/src/components/ProjectDitailsPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <header class="subhead__img">
+            <img class="" src="" alt="">
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectDitailsPage',
+
+    data() {
+        return {
+            subheader: [{
+               class: 'subhead__img_photo',
+               src: require ('./../assets/img/projectDitails/subhead.jpg'),
+               alt: 'subhead img' 
+            }]
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
