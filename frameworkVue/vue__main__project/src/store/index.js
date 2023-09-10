8fbf041a42a628031f5d8ecd113a78a748eacb60
Лилия <l_loshakova123@mail.ru>
2023-09-10 05:39:46 +0300

    Sketch

--- frameworkVue/vue__main__project/src/store/index.js
+++ frameworkVue/vue__main__project/src/store/index.js
@@ -6,6 +6,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         // начальное состояние
+        heading: 'Minimal Look Bedrooms',
+        text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo. ',
+        text2: 'In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque arcu. Donec a pellentesque Cras erat enim, gravida non ante vitae,elequis convallis elit, in viverra felis. Donec ultrices tellus vitae iaculisvd porta. Proin tincidunt ligula id purus porttitor.'
     },
     mutations: {
         // методы для изменения состояния
